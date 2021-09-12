d5e227e665475afe9ad61862d84883aed2982544
f-person <agakaryan.arshak@gmail.com>
2021-09-12 15:16:48 +0400

    refactor: clean up the example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -9,15 +9,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const FlutterPersistentKeyboardHeightProvider(
+    return const MaterialApp(
+      title: 'Flutter Persistent Keyboard Height Example',
+      home: FlutterPersistentKeyboardHeightProvider(
         child: FlutterPersistentKeyboardHeightExample(),
       ),
     );
